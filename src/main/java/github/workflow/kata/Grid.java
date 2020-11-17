e85a960bd1b22474680eb27a17c52f274387f480
Frank Karlstrøm <frank.karlstrom@gmail.com>
2020-11-17 23:53:59 +0100

    Refactored the grid class content
    now use a char[][] as grid, better than before, but still a bit "raw".
    Can probably be more OO, but that can be fixed at a later point in time,
    when more rules are implemented.

--- src/main/java/github/workflow/kata/Grid.java
+++ src/main/java/github/workflow/kata/Grid.java
@@ -1,74 +1,69 @@
 package github.workflow.kata;
 
-import java.util.Objects;
+import java.util.Arrays;
 
 /**
  * A gaming grid.
  */
 public class Grid {
-    private String gridDef;
+    private char[][] grid;
 
+    /**
+     * Extract the grid and store it as a char[][]
+     * Makes sure the gridDef has no spaces, as equals does a string comparison, while the input can have spaces
+     * around the grid definition.
+     */
     public Grid(String gridDef) {
-        // make sure the gridDef has no spaces, as equals does a string comparison, while the input can have spaces
-        // around the grid definition.
-        String niceDef = "";
         String[] rows = gridDef.trim().split("\n");
-        for (String s : rows) {
-            char[] cols = s.trim().toCharArray();
-            for (char c : cols) {
-                niceDef += c;
-            }
-            // add a new-line
-            niceDef += '\n';
+        grid = new char[rows.length][];
+        for (int row = 0; row < rows.length; row++) {
+            grid[row] = rows[row].trim().toCharArray();
         }
-        this.gridDef = niceDef;
     }
 
     public void nextGeneration() {
-        // so, lets try to implement the actual code.
-        // not so happy with the implementation, it's "a bit" dirty, but it works. Refactor this later.
-        String newDef = "";
-        String[] rows = gridDef.split("\n");
-        for (int row = 0; row < rows.length; row++) {
-            char[] cols = rows[row].toCharArray();
-            for (int col = 0; col < cols.length; col++) {
-                char ch = cols[col];
+        int rowCount = grid.length;
+        int colCount = grid[0].length;
+        char[][] newGrid = new char[rowCount][colCount];
+        for (int row = 0; row < rowCount; row++) {
+            for (int col = 0; col < colCount; col++) {
+                char ch = grid[row][col];
                 // Rule: As a live cell I will die if I have fewer than two live neighbours
                 if (ch == '*') {
-                    // check the surrounding 9 cells.
-                    int liveCount = 0;
-                    int startRow = Math.max(0, row - 1);
-                    int endRow = Math.min(rows.length-1, row + 1);
-                    int startCol = Math.max(0, col - 1);
-                    int endCol = Math.min(cols.length-1, col + 1);
-                    for (int i = startRow; i <= endRow; i++) {
-                        for (int j = startCol; j <= endCol; j++) {
-                            // skip ourselves.
-                            if (i == row && j == col) {
-                                continue;
-                            }
-                            if (rows[i].toCharArray()[j] == '*') {
-                                liveCount++;
-                            }
-                        }
-                    }
-                    if (liveCount < 2) {
-                        // cell will die.
-                        newDef += '.';
-                    } else {
-                        // cell will still be alive.
-                        newDef += '*';
-                    }
+                    int liveCount = getLiveCountAround(row, col);
+                    newGrid[row][col] = liveCount < 2 ? '.' : '*';
                 } else {
                     // cell is dead, it will also be dead next round.
-                    newDef += '.';
+                    newGrid[row][col] = '.';
                 }
             }
-            // add a new-line
-            newDef += '\n';
         }
         // now replace old definition with new definition.
-        gridDef = newDef;
+        grid = newGrid;
+    }
+
+    /**
+     * Count the live cells using the surrounding 9 cells.
+     */
+    private int getLiveCountAround(int rowIdx, int colIdx) {
+        int liveCount = 0;
+        int startRow = Math.max(0, rowIdx - 1);
+        int endRow = Math.min(grid.length - 1, rowIdx + 1);
+        int startCol = Math.max(0, colIdx - 1);
+        int endCol = Math.min(grid[0].length - 1, colIdx + 1);
+
+        for (int row = startRow; row <= endRow; row++) {
+            for (int col = startCol; col <= endCol; col++) {
+                // skip ourselves.
+                if (row == rowIdx && col == colIdx) {
+                    continue;
+                }
+                if (grid[row][col] == '*') {
+                    liveCount++;
+                }
+            }
+        }
+        return liveCount;
     }
 
     @Override
@@ -76,11 +71,11 @@ public class Grid {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Grid grid = (Grid) o;
-        return gridDef.equals(grid.gridDef);
+        return Arrays.deepEquals(this.grid, grid.grid);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(gridDef);
+        return Arrays.deepHashCode(grid);
     }
 }
