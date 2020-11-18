cd2053c9bf4d517c7e911cb4f4415e1e238bb7a3
Frank Karlstrøm <frank.karlstrom@gmail.com>
2020-11-18 18:00:25 +0100

    Adjusted BDD test to cover both rule 1 and 2.
    Funny enough, no code changes was necessary, tests was green.
    Also adjusted checkstyle, and fixed the warnings.

--- src/main/java/github/workflow/kata/GOL.java
+++ src/main/java/github/workflow/kata/GOL.java
@@ -1,6 +1,14 @@
 package github.workflow.kata;
 
-public class GOL {
+public final class GOL {
+    private GOL() {
+    }
+
+    /**
+     * Starter for this Game Of Life.
+     *
+     * @param args the commend line arguments this GOL is started with.
+     */
     public static void main(String[] args) {
         System.out.println("Not working yet.");
     }
